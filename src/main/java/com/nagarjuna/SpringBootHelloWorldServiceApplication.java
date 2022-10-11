4b7d25993fdaefd9f7b786b05534235cb37a06e7
NAGARJUNA GOUD TUMMALA <nagarjuna.goud.tummala@kubota.com>
2022-10-11 17:31:43 -0500

    Initial Commit

---
+++ src/main/java/com/nagarjuna/SpringBootHelloWorldServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nagarjuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloWorldServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nagarjuna/controller/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.nagarjuna.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    private static final Logger LOGGER = LogManager.getLogger(HelloWorldController.class);
+
+    // http://localhost:8080/
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> hello() {
+        LOGGER.info("Invoked HelloWorldController.java # hello() method");
+        String message = "Hello Openshift";
+        ResponseEntity<String> responseEntity = new ResponseEntity<String>(message, HttpStatus.OK);
+        LOGGER.info("Exited HelloWorldController.java # hello() method");
+        return responseEntity;
+    }
+}
